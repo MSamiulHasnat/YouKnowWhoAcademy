2704d2442c633b7e109f2655fe81eb4a3a6ea2cf
M. Samiul Hasnat <m.samiulhasnat@Samiuls-MacBook-Air.local>
2022-12-08 23:23:48 +0600

    New Directory

---
+++ AAA_Format.cpp
@@ -0,0 +1,27 @@
+// Problem link - https://atcoder.jp/contests/abc169/tasks/abc169_a?lang=en
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <algorithm>
+
+#define nl '\n'
+
+using namespace std;
+
+int main(){
+{
+//this 4 lines are for input and output .text files
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+}
+// My Code Starts Here         
+    int a, b;
+    cin >> a >> b;
+    cout << a*b;
+
+    return 0;
+}
---
+++ A_Multiplication1.cpp
@@ -0,0 +1,25 @@
+// Problem link - 
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <algorithm>
+
+#define nl '\n'
+
+using namespace std;
+
+int main(){
+{
+//this 4 lines are for input and output .text files
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+}
+// My Code Starts Here         
+    
+
+    return 0;
+}
